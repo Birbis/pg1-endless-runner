f50abd2b60ff8d769a97dfaa5fe01d32a10595c8
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2021-02-11 11:47:13 +0100

    [Controls] - Jump on tap and double Jump

--- Assets/Scripts/Moves/Jump.cs
+++ Assets/Scripts/Moves/Jump.cs
@@ -1,4 +1,5 @@
-﻿using Moves;
+﻿using System;
+using Moves;
 using UnityEngine;
 
 public class Jump : MonoBehaviour, Move {
@@ -9,6 +10,18 @@ public class Jump : MonoBehaviour, Move {
 	Range(1.0f, 10.0f)]
 	private float _jumpForce;
 
+	[SerializeField,
+	Tooltip("The maximum force given to the double jump of the player"),
+	Range(1.0f, 10.0f)]
+	private float _doubleJumpForce;
+
+	[SerializeField,
+	Tooltip("The y velocity that the player can have when starting the double jump"),
+	Range(.1f, 2f)]
+	private float _maxDoubleJumpYVelocity;
+
+	private bool canJumpAgain = true;
+
 	private Rigidbody _rb;
 
 	private void Start() {
@@ -16,8 +29,14 @@ public class Jump : MonoBehaviour, Move {
 		if (_rb == null) Debug.LogWarning("[Jump]::Start - RigidBody not found");
 	}
 
-	public void execute() {
-		_rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+	public void execute(bool isGrounded) {
+		if (isGrounded) {
+			canJumpAgain = true;
+			_rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+		} else if (canJumpAgain && Math.Abs(_rb.velocity.y) <= _maxDoubleJumpYVelocity) {
+			_rb.AddForce(Vector3.up * _doubleJumpForce, ForceMode.Impulse);
+			canJumpAgain = false;
+		}
 	}
 
 	public bool Freezed { get => _freezed; set => _freezed = value; }
