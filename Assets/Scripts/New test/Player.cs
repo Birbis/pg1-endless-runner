f8a764f0b2cd24f217ab2d3bdb1879fc544097d3
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2021-03-04 11:43:36 +0100

    [Movement] - Added crouch

--- Assets/Scripts/New test/Player.cs
+++ Assets/Scripts/New test/Player.cs
@@ -7,18 +7,35 @@ public class Player : MonoBehaviour {
 
 	private CharacterController _controller;
 
+	#region Movement variables
 	[SerializeField]
 	private float _speed = 5.0f;
 	[SerializeField]
 	private float _jumpHeight = 10.0f;
+	#endregion
+
+	#region Jump variables
 	[SerializeField]
 	private float _gravity = 1.0f;
 	[SerializeField]
 	private float _maxDoubleJumpVelocity = 5.0f;
-
 	private float _yVelocity = 0.0f;
 	private bool _jumpInput = false;
 	private bool _canDoubleJump = false;
+	#endregion
+
+	#region Crouch variables
+	[SerializeField]
+	private float _crouchResize = 1.0f;
+
+	[SerializeField]
+	private float _crouchDuration = 1.0f;
+
+	private float _originalControllerheight;
+	private bool _canCrouch = true;
+	#endregion
+
+
 
 	// Start is called before the first frame update
 	void Start() {
@@ -26,7 +43,7 @@ public class Player : MonoBehaviour {
 		#region Events subscription
 		// Subscribe to swiping functionalities
 		// SwipeController.Instance.onSwipeUp += OnSwipeUp;
-		// SwipeController.Instance.onSwipeDown += OnSwipeDown;
+		SwipeController.Instance.onSwipeDown += OnSwipeDown;
 		// SwipeController.Instance.onSwipeLeft += OnSwipeLeft;
 		// SwipeController.Instance.onSwipeRight += OnSwipeRight;
 		SwipeController.Instance.onTap += OnTap;
@@ -67,5 +84,20 @@ public class Player : MonoBehaviour {
 
 	private void OnTap() {
 		_jumpInput = true;
+	}
+
+	private void OnSwipeDown() {
+		if (_canCrouch) {
+			_canCrouch = false;
+			_originalControllerheight = _controller.height;
+			_controller.height = _crouchResize;
+			StartCoroutine("Crouch");
+		}
+	}
+
+	private IEnumerator Crouch() {
+		yield return new WaitForSeconds(_crouchDuration);
+		_controller.height = _originalControllerheight;
+		_canCrouch = true;
 	}
 }
